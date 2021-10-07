a5ed09dc7e9294f7a4c1f41b64e26d6bb01d7497
Ryotruuser <90880803+Ryotruuser@users.noreply.github.com>
2021-10-06 21:42:46 -0300

    Add files via upload

---
+++ js/script.js
@@ -0,0 +1,360 @@
+var deck = {
+  carta1: {
+    nome: "Pikachu",
+   imagem: "https://i.pinimg.com/originals/51/a6/bc/51a6bc8a9107cb0319807b4c0b0812ae.gif",
+   defeat: "https://c.tenor.com/lmA7VALYIAsAAAAC/sad-pikachu.gif",
+   atributos: {
+     ataque: 100,
+     defesa: 30,
+     Mental: 20
+   }
+  },
+  
+  carta2:  {
+    nome : "God Goku",
+    imagem: "https://giffiles.alphacoders.com/131/13142.gif" , 
+    defeat: "http://pa1.narvii.com/6826/bf3a7cd931ffaeaa24ec0a98f360d2f1e6eb2053_00.gif" ,
+    atributos: {
+     ataque: 147,
+     defesa: 50,
+     Mental: 70
+   }
+  },
+  
+  carta3: {
+    nome : "Sasuke",
+    imagem: "https://www.icegif.com/wp-content/uploads/sasuke-icegif-1.gif",
+    defeat: "https://c.tenor.com/zJAnoewSzb8AAAAd/naruto.gif",
+    atributos: {
+     ataque: 140,
+     defesa: 90,
+     Mental: 30
+   }   
+  },
+  carta4: {
+    nome: "Killua",
+   imagem: "https://media2.giphy.com/media/8DTnuPhxv0m4w/giphy.gif",
+   defeat: "https://i.pinimg.com/originals/96/8d/94/968d942669999b98e4dd80e2c2a4fba6.gif",
+   atributos: {
+     ataque: 110,
+     defesa: 40,
+     Mental: 70
+   }
+  },
+  carta5: {
+    nome: "Naruto",
+   imagem: "https://i0.wp.com/66.media.tumblr.com/a2457858e72585329ac4e84ac84a0f21/tumblr_n4nufi3nM01tzj388o2_250.gif",
+   defeat: "https://cdn.streamelements.com/uploads/895a8870-0c6c-4bc8-a259-98af795782ee.gif",
+   atributos: {
+     ataque: 120,
+     defesa: 50,
+     Mental: 50
+   }
+  },
+  carta6: {
+    nome: "Kurapika",
+   imagem: "https://i.pinimg.com/originals/ee/25/a1/ee25a1df7af1bdf58b575eeedd631a3a.gif",
+   defeat: "https://64.media.tumblr.com/0ff4805dcf5c641aa74b24ed7b920103/86c38f2cfeb76540-7f/s500x750/39d88f4a9d9d5b742d364a1e357ad3411264ea3d.gif",
+   atributos: {
+     ataque: 125,
+     defesa: 65,
+     Mental: 15
+   }
+  },
+  carta7: {
+    nome: "Saitama",
+   imagem: "https://i.pinimg.com/originals/ea/a7/16/eaa716dfde124900277b9b8e5f2c756a.gif",
+   defeat: "https://c.tenor.com/uM5_PN283uUAAAAC/pissed-saitama.gif",
+   atributos: {
+     ataque: 160,
+     defesa: 70,
+     Mental: 10
+   }
+  },
+  carta8: {
+    nome: "Tenma de Pégaso",
+   imagem: "https://pa1.narvii.com/6348/7ec3538fd5f687055d7e9abe6e171f2ac2ff439d_hq.gif",
+   defeat: "https://i.pinimg.com/originals/d3/db/3e/d3db3eecb75cfb32c61aced5f8740d94.gif",
+   atributos: {
+     ataque: 90,
+     defesa: 30,
+     Mental: 25
+   }
+  },
+  carta9: {
+    nome: "Zeref",
+   imagem: "https://c.tenor.com/wetkOZ0E1o8AAAAC/zeref-dragneel-fairy-tail.gif",
+   defeat: "https://i0.wp.com/media1.tenor.com/images/9ccddf7ce5df465809f16c888bf3ed7f/tenor.gif",
+   atributos: {
+     ataque: 100,
+     defesa: 100,
+     Mental: 80
+   }
+  },
+  carta10: {
+    nome: "Erza",
+   imagem: "https://38.media.tumblr.com/91390b44f3a91bf5d3596617a81814d8/tumblr_mrp2icMkBe1scheqto1_500.gif",
+   defeat: "https://c.tenor.com/t1mSj6XBI7gAAAAC/fairy-tail-erza.gif",
+   atributos: {
+     ataque: 130,
+     defesa: 70,
+     Mental: 40
+   }
+  },
+  carta11: {
+    nome: "Urushibara Shizuno",
+   imagem: "https://pa1.narvii.com/6708/e98bc332900629f95be73a8efcd7115247d57d2e_hq.gif",
+   defeat: "https://i.pinimg.com/originals/b2/7e/c5/b27ec50437f21bd08606947be4a0c5ea.gif",
+   atributos: {
+     ataque: 60,
+     defesa: 80,
+     Mental: 50
+   }
+  },
+  carta12: {
+    nome: "Chika Amatori",
+   imagem: "https://64.media.tumblr.com/1162e883fff3854c001bd99218769835/e60fb0b8831229fb-9c/s500x750/d6358eadad0cc7964ebd1f6fc747fcb125469b51.gif",
+   defeat: "https://64.media.tumblr.com/0544d84a174130f84d084cc85cdaa2f4/tumblr_nolb8yfxqK1unn7t4o4_500.gif",
+   atributos: {
+     ataque: 90,
+     defesa: 80,
+     Mental: 10
+   }
+  },
+  carta13: {
+    nome: "kuga yuma",
+   imagem: "https://static.wikia.nocookie.net/worldtrigger/images/6/65/Yuma%27s_armor.gif",
+   defeat: "https://c.tenor.com/qix2X5XHZ74AAAAC/kuga-yuma-world-trigger.gif",
+   atributos: {
+     ataque: 110,
+     defesa: 40,
+     Mental: 70
+   }
+  },
+  carta14: {
+    nome: "Shiroe",
+   imagem: "https://31.media.tumblr.com/16c5126fef72592182409a229621983a/tumblr_n2whpyRT6z1tollfpo1_1280.gif",
+   defeat: "https://64.media.tumblr.com/f34095795ef4989e9fe8c2754f315046/196109816096a7fa-1d/s500x750/511ad34b50a752ab7b982b94da24767f97d869c0.gif",
+   atributos: {
+     ataque: 100,
+     defesa: 20,
+     Mental: 150
+   }
+  },
+  carta15: {
+    nome: "Yui",
+   imagem: "https://i0.wp.com/i.pinimg.com/originals/37/3c/38/373c38c9ec024c3064f82dc44384b24b.gif",
+   defeat: "https://i.pinimg.com/originals/6b/ec/d0/6becd0bfc0c70ecfdc63dd739b408245.gif",
+   atributos: {
+     ataque: 130,
+     defesa: 70,
+     Mental: 40
+   }
+  },
+  carta16: {
+    nome: "Carrot",
+   imagem: "https://pa1.narvii.com/7528/d75602aee0be7dc095de0444a8163c6e55bcd5e0r1-420-236_hq.gif",
+   defeat: "http://pa1.narvii.com/6783/68b1c63ecd0cc9e830a71ea53fc3f910db7f6938_00.gif",
+   atributos: {
+     ataque: 100,
+     defesa: 40,
+     Mental: 20
+   }
+  },
+  carta17: {
+    nome: "Saber",
+   imagem: "http://www.magazine-hd.com/apps/wp/wp-content/uploads/2017/02/saber2.gif",
+   defeat: "https://i.pinimg.com/originals/d0/8f/86/d08f863f5081bf91537b31e924ba8103.gif",
+   atributos: {
+     ataque: 135,
+     defesa: 30,
+     Mental: 40
+   }
+  },
+  carta18: {
+    nome: "Megumin",
+   imagem: "https://c.tenor.com/LPv6mAowVb8AAAAC/megumin-anime.gif",
+   defeat: "http://pa1.narvii.com/6453/08228063b9ba725e2b29c86e95332bed71855ab9_00.gif",
+   atributos: {
+     ataque: 100,
+     defesa: 10,
+     Mental: 20
+   }
+  },
+  carta19: {
+    nome: "Nana Shimura",
+   imagem: "https://i.pinimg.com/originals/ac/5a/a2/ac5aa27ba23615cb706b39ab9483e70e.gif",
+   defeat: "https://i.pinimg.com/originals/9f/95/aa/9f95aaa5528dfe4dbadcaeaa15aaebeb.gif",
+   atributos: {
+     ataque: 120,
+     defesa: 40,
+     Mental: 20
+   }
+  },
+  carta20: {
+    nome: "Feitan",
+   imagem: "https://pa1.narvii.com/6338/83468aa00f24f022f63c18aac1aba1a7b6a9f1a5_hq.gif",
+   defeat: "https://c.tenor.com/XA1asX69aCIAAAAC/feitan-hx-h.gif",
+   atributos: {
+     ataque: 130,
+     defesa: 70,
+     Mental: 40
+   }
+  },
+  carta21: {
+    nome: "Yami Yugi",
+   imagem: "https://pa1.narvii.com/7698/5bd15d85d02120f9065b9c510c239c64ca880c5dr1-540-304_hq.gif",
+   defeat: "https://64.media.tumblr.com/0f2132900115f8fe3479336124d3ee91/125752c24dee0b7d-dd/s500x750/17a9662cac0b25c615a2f93cc4b4b503cc6afb4d.gif",
+   atributos: {
+     ataque: 127,
+     defesa: 30,
+     Mental: 80
+   }
+  },
+  carta22: {
+    nome: "Toshinori",
+   imagem: "https://c.tenor.com/70lmsU9jSxgAAAAC/all-might-young.gif",
+   defeat: "https://i.pinimg.com/originals/d4/85/a4/d485a4e66144ae83954163a84af9332a.gif",
+   atributos: {
+     ataque: 150,
+     defesa: 40,
+     Mental: 30
+   }
+  },
+  
+}
+
+
+var cartas = [deck.carta1, deck.carta2, deck.carta3,deck.carta4,deck.carta5,deck.carta6,deck.carta6
+             ,deck.carta7,deck.carta8,deck.carta9,deck.carta10,deck.carta11,deck.carta12
+             ,deck.carta13,deck.carta14,deck.carta15,deck.carta16,deck.carta17,deck.carta18
+             ,deck.carta19,deck.carta20,deck.carta21,deck.carta22];
+var cartaMaquina;
+var cartaJogador;
+
+function sortearCarta(){
+  var rngMaquina = parseInt(Math.random()*22);
+  cartaMaquina = cartas [rngMaquina];
+  var divWin = document.getElementById("win")
+  var divDefeat  = document.getElementById("defeat")
+  var divDraw = document.getElementById("draw")
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  
+  var rngJogador = parseInt(Math.random()*22);
+  while(rngMaquina == rngJogador){
+    rngJogador = parseInt(Math.random()*22);
+  }
+  
+  
+  var tagHTML = "<div id='opcoes' class='carta-status'>"
+  var opcoesTexto = ""
+  var nome = `<p class="carta-subtitle"> </p>`   
+  var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;>';
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>"
+  divCartaMaquina.style.backgroundImage = 'none'
+  
+  cartaJogador = cartas [rngJogador];
+ 
+  document.getElementById("btnSortear").disabled = true;
+  document.getElementById("btnJogar").disabled = false;
+  divDefeat.style.display = 'none'
+  divWin.style.display = 'none'
+  divDraw.style.display = 'none'
+  exibirCartaJogador();
+}
+
+function pegarAtributo (){
+  var radioAtributos = document.getElementsByName("atributo");
+  
+  for(var i = 0; i < radioAtributos.length; i++){
+    if (radioAtributos[i].checked == true){
+      return radioAtributos[i].value
+    }
+  }
+ 
+  
+}
+
+function jogar(){
+  var atributoInvocado = pegarAtributo()
+  var divWin = document.getElementById("win")
+  var divDefeat  = document.getElementById("defeat")
+  var divDraw = document.getElementById("draw")
+  var divCartaJogador = document.getElementById("carta-maquina");
+  var divCartaMaquina = document.getElementById("carta-jogador");
+  
+  
+  if (cartaJogador.atributos[atributoInvocado] > cartaMaquina.atributos[atributoInvocado]){
+    divCartaJogador.style.backgroundImage = `url(${cartaMaquina.defeat})`
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo- transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class='carta-status'>"
+  
+    divCartaMaquina.style.backgroundImage = `url(${cartaJogador.imagem})`
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo- transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class='carta-status'>"
+    divDefeat.style.display = 'none'
+    divWin.style.display = 'block'
+    divWin.innerHTML= "<p class='win'>Você venceu!</p>"
+    
+    
+  }
+  else if (cartaMaquina.atributos[atributoInvocado] > cartaJogador.atributos[atributoInvocado] ){
+    divCartaJogador.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo- transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class='carta-status'>"
+    
+    divCartaMaquina.style.backgroundImage = `url(${cartaJogador.defeat})`
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo- transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class='carta-status'>"
+    divWin.style.display = 'none'
+    divDefeat.style.display = 'block'
+    divDefeat.innerHTML= "<p class='defeat'>Derrota</p>"
+    
+  }
+  else {
+    divCartaMaquina.style.backgroundImage = `url(${cartaJogador.imagem})`
+    divCartaJogador.style.backgroundImage = `url(${cartaMaquina.imagem})`
+    var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo- transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+    var tagHTML = "<div id='opcoes' class='carta-status'>"
+    divWin.style.display = 'none'
+    divDefeat.style.display = 'none'
+    divDraw.style.display = 'block'
+    divDraw.innerHTML= "<p class='draw'>Empatou</p>"
+  }
+  
+  document.getElementById('btnJogar').disabled = true;
+  document.getElementById("btnSortear").disabled = false;
+  exibirCartaMaquina();
+  
+}
+
+function exibirCartaJogador(){
+  var divCartaJogador = document.getElementById("carta-jogador");
+  divCartaJogador.style.backgroundImage = `url(${cartaJogador.imagem})`
+  var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>"
+  
+  var opcoesTexto = ""
+  for(var atributo in cartaJogador.atributos){
+    opcoesTexto += `<input class="radio" type="radio" name="atributo" value="${atributo}"> ${atributo} ${cartaJogador.atributos[atributo]}<br>`
+  }
+  var nome = `<p class="carta-subtitle"> ${cartaJogador.nome}</p>`   
+
+  divCartaJogador.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>"
+  
+}
+
+function exibirCartaMaquina(){
+  var divCartaMaquina = document.getElementById("carta-maquina");
+  var moldura = '<img src="https://www.alura.com.br/assets/img/imersoes/dev-2021/card-super-trunfo-transparent-ajustado.png" style=" width: inherit; height: inherit; position: absolute;">';
+  var tagHTML = "<div id='opcoes' class='carta-status'>"
+  
+  var opcoesTexto = ""
+  for(var atributo in cartaJogador.atributos){
+    opcoesTexto += `<p type="text" name="atributo" value="${atributo}" required> ${atributo}  ${cartaMaquina.atributos[atributo]} </p>`
+  }
+  var nome = `<p class="carta-subtitle"> ${cartaMaquina.nome}</p>`   
+
+  divCartaMaquina.innerHTML = moldura + nome + tagHTML + opcoesTexto + "</div>"
+  
+}
